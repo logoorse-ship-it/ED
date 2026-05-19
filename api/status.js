4127b483996ed046706860a57c44e5ee2ec45128
logoorse-ship-it <logoorse@gmail.com>
2026-05-19 19:34:13 -0400

    edit

--- api/status.js
+++ api/status.js
@@ -1,25 +1,18 @@
+import { KlingAPI } from 'kling-api';
+
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   
   const { taskId } = req.query;
-  const AK = process.env.KLING_ACCESS_KEY;
-  const SK = process.env.KLING_SECRET_KEY;
 
   try {
-    const { SignJWT } = await import('jose');
-    const secret = new TextEncoder().encode(SK);
-    const jwt = await new SignJWT({ iss: AK })
-      .setProtectedHeader({ alg: 'HS256' })
-      .setExpirationTime('30m')
-      .setNotBefore('0s')
-      .sign(secret);
-
-    const response = await fetch(`https://api.klingai.com/v1/videos/text2video/${taskId}`, {
-      headers: { 'Authorization': `Bearer ${jwt}` }
+    const api = new KlingAPI({
+      accessKey: process.env.KLING_ACCESS_KEY,
+      secretKey: process.env.KLING_SECRET_KEY
     });
 
-    const data = await response.json();
-    res.status(200).json(data);
+    const result = await api.getVideoTask(taskId);
+    res.status(200).json(result);
   } catch(err) {
     res.status(500).json({ error: err.message });
   }
